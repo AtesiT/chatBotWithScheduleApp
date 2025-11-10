d221ff0cff3971c3de2c174f78ad44ef8a6a57fe
AtesiT <atesit@mail.ru>
2025-11-10 17:16:12 +0300

    made UI of app

--- obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7d2b31057049bf798cf893a80c93ebbc04944a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+867db81dc7e8a32a47a576c91f5d8e32ecb9e6a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
